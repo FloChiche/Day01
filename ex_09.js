e7151e3cc449d948301beafced2616b4df8f71b8
FloChiche <florian.chicheportiche@epitech.digital>
2024-09-30 16:40:21 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,18 +1,6 @@
-let type;
-if (typeof input_var === 'string') {
-    type = 'String';
-} else if (typeof input_var === 'boolean') {
-    type = 'Boolean';
-} else if (typeof input_var === 'number') {
-    if (Number.isInteger(input_var)) {
-        type = 'Integer';
-    } else {
-        type = 'Float';
-    }
-}
-
-displayThisText(`The variable value is' + input_var);
-displayThisText(`Its type is' + type);
-    if (input_var = 42) {
-     displayThisText("It is the meaning of life.");
+displayThisText("The variable value is" + "" + input_var + "" + ".");
+displayThisText("Its type is" + "" + typeof(input_var)+ " " + ".");
+if (input_var ==42)
+{
+	displayThisText("It is the meaning of life.")
 }
