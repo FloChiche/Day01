455f868997a20c7a6b0536706086cde5226e056f
FloChiche <florian.chicheportiche@epitech.digital>
2024-09-30 16:40:53 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,4 +1,4 @@
-displayThisText("The variable value is" + "" + input_var + "" + ".");
+displayThisText("The variable value is" + " " + input_var + " " + ".");
 displayThisText("Its type is" + "" + typeof(input_var)+ " " + ".");
 if (input_var ==42)
 {
